081c22bfca689fe0e97c2257225cf3059851a238
PixelOmen <PixelOmen@gmail.com>
2024-06-26 16:02:18 -0700

    chore: removed testing dir

--- testing/testing.py
+++
@@ -1,9 +0,0 @@
-from src.scanning import Scanner
-
-testpath = r"C:\Users\eman\Projects\python\alulamoat"
-
-scanner = Scanner()
-scanner.setroot(testpath)
-scanner.scan(excludedirs=["__pycache__"], dironly=False)
-if scanner.results:
-    print(len(scanner.results.dirs))
